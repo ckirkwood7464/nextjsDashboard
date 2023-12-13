2a7fad35dccea8b724b7db19704d657c5f75a8a7
Connor Kirkwood <ckirkwood7464@gmail.com>
2023-12-13 14:42:00 -0500

    fixed alt again

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -27,11 +27,11 @@ export default async function InvoicesTable({
                   <div>
                     <div className="mb-2 flex items-center">
                       <Image
+                        alt='profile picture'
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt='profile picture'
                       />
                       <p>{invoice.name}</p>
                     </div>
